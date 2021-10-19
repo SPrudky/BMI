ddd9638b0cda8b987f3879c596135b6de4b0146d
Samuel Prudký <Prudky.Samuel@stud.vassboskovice.cz>
2021-10-19 11:27:20 +0200

    hotovo

--- BMI/Program.cs
+++ BMI/Program.cs
@@ -6,7 +6,38 @@ namespace BMI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double hmotnost;
+            double výška;
+            double BMI;
+
+
+            Console.WriteLine("Zadejte hmotnost(kg)");
+            Double.TryParse(Console.ReadLine(), out hmotnost);
+            if (hmotnost <= 0)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Pouze kladná hodnota je povolena");
+            }
+            else
+            {
+                Console.WriteLine("Zadejte výšku(cm)");
+                výška = double.Parse(Console.ReadLine());
+                if (výška <= 0)
+                {
+                    Console.WriteLine("Pouze kladná hodnota je povolena");
+                }
+                else
+                {
+                    BMI = hmotnost / Math.Pow(výška / 100, 2);
+                    BMI = Math.Round(BMI);
+
+                    Console.WriteLine();
+                    Console.WriteLine($"Vaše BMI je {BMI} ");
+                }
+            }
+            Console.ReadLine();
+
         }
+
     }
 }
